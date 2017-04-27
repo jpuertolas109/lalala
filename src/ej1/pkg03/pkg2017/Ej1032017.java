ec3278b54b31190a73d8d872e483ac4c5f4fbb32
Alumno <Alumno@INF109>
2017-04-27 14:15:31 +0200

    mimimimi

---
+++ src/ej1/pkg03/pkg2017/Ej1032017.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ej1.pkg03.pkg2017;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Ej1032017 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ventana V =new Ventana();
+        V.setVisible(true);
+    }
+    
+}
